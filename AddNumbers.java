f73222ed0dbc857f48c0a44fecaf66f55d7c3a74
Naveennicks <naveenrema@gmail.com>
2022-03-22 14:37:26 +0530

    new code

---
+++ AddNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class AddNumbers
+{
+public static void main(String[] args)
+{
+int x,y,z;
+System.out.println("Enter two integers to calculate theirsum;");
+x=in.nextint();
+y=in.nextint();
+z=x+y;
+System.out.println("sum of entered integers="+z);
+}}
