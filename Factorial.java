d7d519c6a38e2891c4192015666b9b6753de2e11
Naveennicks <naveenrema@gmail.com>
2022-03-27 14:26:37 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package tamilnadu.chennai;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int no=7;
+		int fact =no;
+		while(no>1)
+		{
+			no--;
+			fact =fact*no;
+		}System.out.println(fact);
+
+	}
+
+}
